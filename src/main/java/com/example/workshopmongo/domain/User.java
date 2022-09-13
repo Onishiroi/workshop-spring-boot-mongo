d5975aed7d9bffe398a88f915f09410e532fb217
Moisés Pereira <moiseseletronica@gmail.com>
2022-09-12 22:26:07 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/example/workshopmongo/domain/User.java
+++ src/main/java/com/example/workshopmongo/domain/User.java
@@ -3,10 +3,17 @@ package com.example.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection="user") //se não colocar o nome, por padrão pega o nome da classe em lowercase, logo, nem precisava neste caso. Usei apenas para exemplo de possibilidade
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private String id;
+	
 	private String name;
 	private String email;
 
@@ -15,6 +22,8 @@ public class User implements Serializable {
 
 	public User(String id, String name, String email) {
 		super();
+		
+
 		this.id = id;
 		this.name = name;
 		this.email = email;
